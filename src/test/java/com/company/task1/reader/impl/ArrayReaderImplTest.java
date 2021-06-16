0aa03fbf65bec897c1a5f28a9a2d22eac01e72ff
Kuziatihii <61865176+Kuziatihii@users.noreply.github.com>
2021-06-16 10:55:42 +0300

    clean code

--- src/test/java/com/company/task1/reader/impl/ArrayReaderImplTest.java
+++ src/test/java/com/company/task1/reader/impl/ArrayReaderImplTest.java
@@ -9,7 +9,7 @@ public class ArrayReaderImplTest {
 
     @Test
     public void testStringArray() throws ArrayException {
-        final String FILE_NAME = "src\\main\\resources\\data\\arrayFile.txt";
+        final String FILE_NAME = "src/main/resources/data/arrayFile.txt";
         ArrayReaderImpl instance = new ArrayReaderImpl();
         String expResult = "10 -15 83 47 -1 0 3 0 57";
         String actual = instance.stringArray(FILE_NAME);
